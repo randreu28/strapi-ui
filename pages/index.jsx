8a5deb8f066511f0c47326ce956ea9d06ca35b5f
randreu28 <randreu28@gmail.com>
2022-07-26 23:49:44 +0200

    Makes an initial api testing call

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1 className="text-8xl text-blue-500">Hello world</h1>;
+import { fetcher } from "../lib/fetcher";
+
+export default function Home({ landing }) {
+  return (
+    <>
+      <h1 className="text-8xl text-blue-500">{landing.title}</h1>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const landingResponse = await fetcher("/landing");
+  return {
+    props: {
+      landing: landingResponse.data.attributes,
+    },
+  };
 }
