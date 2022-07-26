bce5ad50b9511e2dc9b91b1ae7429c53a710d6ba
randreu28 <randreu28@gmail.com>
2022-07-26 23:38:50 +0200

    Implements formatting standard

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className="text-8xl text-blue-500">Hello world</h1>
-  )
-}
---
+++ pages/index.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-8xl text-blue-500">Hello world</h1>;
+}
