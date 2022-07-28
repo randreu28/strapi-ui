a1a3408dcb65952e0796668ab1ce827d14f64d13
randreu28 <randreu28@gmail.com>
2022-07-28 17:57:24 +0200

    Changes landing design

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ import { MDXRemote } from "next-mdx-remote";
 
 export default function Home({ landing }) {
   console.log(landing);
-  const [coolButton, setcoolButton] = useState("Cool strapi button");
+  const [coolButton, setcoolButton] = useState("Cool button");
 
   function confeti() {
     const jsConfetti = new JSConfetti();
@@ -16,20 +16,20 @@ export default function Home({ landing }) {
   }
 
   return (
-    <div className="flex h-screen">
-      <div className="m-auto space-y-10 text-gray-800 dark:text-white text-center">
-        <h1 className="text-5xl font-bold">{landing.title}</h1>
-        <div className="prose dark:prose-invert">
-          <MDXRemote {...landing.description} />
-        </div>
-        <button
-          disabled={landing.isStrapyCool ? false : true}
-          className="rounded bg-gray-800 dark:bg-white dark:text-gray-800 text-white px-3 py-2 duration-200 hover:bg-blue-500 disabled:hover:cursor-not-allowed"
-          onClick={confeti}
-        >
-          {coolButton}
-        </button>
+    <div className="m-auto space-y-10 container px-10 pb-10">
+      <h1 className="text-5xl font-bold text-gray-800 dark:text-white text-center mt-10">
+        {landing.title}
+      </h1>
+      <div className="prose dark:prose-invert max-w-none prose-blockquote:border-gray-300 prose-blue prose-li:marker:text-gray-500">
+        <MDXRemote {...landing.description} />
       </div>
+      <button
+        disabled={landing.isStrapyCool ? false : true}
+        className="flex mx-auto rounded bg-gray-800 dark:bg-white dark:text-gray-800 text-white px-3 py-2 duration-200 hover:bg-blue-500 disabled:hover:cursor-not-allowed"
+        onClick={confeti}
+      >
+        {coolButton}
+      </button>
     </div>
   );
 }
