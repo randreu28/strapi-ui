765bf7a62ccf024c5297f3d959dbde203c3ce3fb
randreu28 <randreu28@gmail.com>
2022-07-28 17:10:22 +0200

    Implements mdx-remote for landing description

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,9 @@ import { fetcher } from "../lib/fetcher";
 import JSConfetti from "js-confetti";
 import { useState } from "react";
 
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+
 export default function Home({ landing }) {
   console.log(landing);
   const [coolButton, setcoolButton] = useState("Cool strapi button");
@@ -16,7 +19,7 @@ export default function Home({ landing }) {
     <div className="flex h-screen">
       <div className="m-auto space-y-10 text-gray-800 dark:text-white text-center">
         <h1 className="text-5xl font-bold">{landing.title}</h1>
-        <p className="max-w-lg m-auto">{landing.description}</p>
+        <MDXRemote {...landing.description} />
         <button
           disabled={landing.isStrapyCool ? false : true}
           className="rounded bg-gray-800 dark:bg-white dark:text-gray-800 text-white px-3 py-2 duration-200 hover:bg-blue-500 disabled:hover:cursor-not-allowed"
@@ -31,6 +34,12 @@ export default function Home({ landing }) {
 
 export async function getServerSideProps() {
   const landingResponse = await fetcher("/landing");
+
+  const mdxSource = await serialize(
+    landingResponse.data.attributes.description
+  );
+  landingResponse.data.attributes.description = mdxSource;
+
   return {
     props: {
       landing: landingResponse.data.attributes,
